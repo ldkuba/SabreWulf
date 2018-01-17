af33f8b5a7a7665b70f7ca854c7fe8422f3f9a90
bhm699 <bhm699@cs.bham.ac.uk>
2018-01-17 15:21:14 +0000

    changed state manager

--- src/engine/StateManager.java
+++ src/engine/StateManager.java
@@ -1,28 +1,51 @@
 package engine;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /*
  * Game State Manager - rather self explanatory name
  */
 public class StateManager {
-	GameStates state;
+	ArrayList<AbstractState> registered;
+	private GameState activeState;
 	
-	public StateManager(GameStates state){
-		this.state = state;
+	public StateManager(){
+		registered = new ArrayList<AbstractState>();
 	}
 	
-	private enum GameStates {
-		//list of all of the different states in the game 
-		MAIN_MENU, LOBBY
+	private enum GameState {
+		MENU, LOBBY, GAME, QUIT
 	}
 	
-	private void GameStateUpdate(){
-		/*Load the correct game state according to the user request - each 
-		  case statement would call the update method of the relevant state	*/	
-		switch(state){
-		case LOBBY:
-			break;
-		case MAIN_MENU:
-			break;
+	private void addState(AbstractState newState){
+		registered.add(newState);		
+	}
+	
+	private void printStates(){
+		//print registered states - for testing 
+		Iterator<AbstractState> itr = registered.iterator();
+		while(itr.hasNext()){
+			String elem = itr.next().toString();
+			System.out.print(elem + " /n");
+		}
+	}
+	
+	private boolean isRegistered(AbstractState state){
+		//checks to see if given state is registered
+		if(registered.contains(state)){
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	private void switchToState(AbstractState state){
+		//switch to a specified game state if it exists
+		if (isRegistered(state)){
+			state.init();
+		} else {
+			System.err.println("Cannot switch to state, it does not exist");
 		}
 	}
 }
