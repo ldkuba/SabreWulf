5630aa1d76c37dca3a88b2557ef5a08a2ee8d16e
Jonas Munoz <jxm789@student.bham.ac.uk>
2018-01-24 13:40:46 +0000

    Fixed Client

--- src/engine/client/Client.java
+++ src/engine/client/Client.java
@@ -3,23 +3,36 @@ import java.net.*;
 
 class Client extends Thread
 {
-    
+
     {
         int i;
         BufferedReader inFromUser =
                 new BufferedReader(new InputStreamReader(System.in));
-        DatagramSocket clientSocket = new DatagramSocket();
-        InetAddress IPAddress = InetAddress.getByName("localhost");
-        byte[] sendData = new byte[1024];
-        byte[] receiveData = new byte[1024];
-        String sentence = inFromUser.readLine();
-        sendData = sentence.getBytes();
-        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
-        clientSocket.send(sendPacket);
-        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-        clientSocket.receive(receivePacket);
-        String modifiedSentence = new String(receivePacket.getData());
-        System.out.println("FROM SERVER:" + modifiedSentence);
-        clientSocket.close();
+        DatagramSocket clientSocket;
+		try {
+			clientSocket = new DatagramSocket();
+			 InetAddress IPAddress = InetAddress.getByName("localhost");
+		        byte[] sendData = new byte[1024];
+		        byte[] receiveData = new byte[1024];
+		        String sentence = inFromUser.readLine();
+		        sendData = sentence.getBytes();
+		        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 6666);
+		        clientSocket.send(sendPacket);
+		        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+		        clientSocket.receive(receivePacket);
+		        String modifiedSentence = new String(receivePacket.getData());
+		        System.out.println("FROM SERVER:" + modifiedSentence);
+		        clientSocket.close();
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+ 
     }
 }
---
+++ src/engine/client/startClient.java
@@ -0,0 +1,11 @@
+
+public class startClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Client client = new Client();
+		client.start();
+
+	}
+
+}
