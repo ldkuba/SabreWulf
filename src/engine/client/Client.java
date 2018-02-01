cce4b0b61fc636b48425f0f4594d3372e4d90bca
jan612 <jan612@cs.bham.ac.uk>
2018-02-01 20:31:16 +0000

    renamed ClientStart to Client and changed the main function to a constructor

---
+++ src/engine/client/Client.java
@@ -0,0 +1,10 @@
+package engine.client;
+
+public class Client
+{
+	public Client()
+	{
+		ClientCore cc = new ClientCore();
+		cc.run();
+	}
+}
--- src/engine/client/ClientStart.java
+++
@@ -1,8 +0,0 @@
-package engine.client;
-
-public class ClientStart {
-    public static void main(String args[]){
-        ClientCore cc = new ClientCore();
-        cc.run();
-    }
-}
