c6811427f0a5bd4fa601eaf61fb87ae4054022d3
jan612 <jan612@cs.bham.ac.uk>
2018-02-01 16:20:31 +0000

    working on renderable2D

--- src/graphics/renderer/Renderable2D.java
+++ src/graphics/renderer/Renderable2D.java
@@ -3,13 +3,14 @@ package graphics.renderer;
 import java.nio.ByteBuffer;
 
 import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL11;
 
 import engine.graphics.IndexBuffer;
 import engine.graphics.VertexArray;
 import engine.graphics.VertexAttribute;
 import engine.graphics.VertexBuffer;
-import engine.graphics.VertexLayout;
 import engine.graphics.VertexBuffer.VertexBufferUsage;
+import engine.graphics.VertexLayout;
 import engine.graphics.VertexLayout.AttributeTypes;
 import engine.graphics.shader.ShaderProgram;
 import engine.graphics.texture.Texture;
@@ -19,6 +20,7 @@ public class Renderable2D
 {
 	private final int VERTEX_COUNT = 4;
 	private final int INDEX_COUNT = 6;
+	private final int VERTEX_PRIMITIVE_ATTRIB_COUNT = 3 + 2 + 4;
 	
 	private static VertexLayout s_VertexLayout;
 	private float m_Width, m_Height;
@@ -36,27 +38,32 @@ public class Renderable2D
 		m_VAO = new VertexArray();
 		m_VAO.bind();
 		
+		m_Color = new Vec4(0.0f, 0.0f, 0.0f, 1.0f);
+		
 		float[] vertices = new float[] {
-			
+			width, 0.0f, 1.0f, 1.0f, 0.0f, m_Color.getW(), m_Color.getX(), m_Color.getY(), m_Color.getZ(),
+			0.0f, 0.0f, 1.0f, 0.0f, 0.0f, m_Color.getW(), m_Color.getX(), m_Color.getY(), m_Color.getZ(),
+			0.0f, height, 1.0f, 1.0f, 0.0f, m_Color.getW(), m_Color.getX(), m_Color.getY(), m_Color.getZ(),
+			width, height, 1.0f, 1.0f, 1.0f, m_Color.getW(), m_Color.getX(), m_Color.getY(), m_Color.getZ()
 		};
 		
 		ByteBuffer vertexBuffer = BufferUtils.createByteBuffer(s_VertexLayout.getVertexSizeInBytes() * 8);
-		for(int i = 0; i < 8 * 7; i++)
+		for(int i = 0; i < VERTEX_PRIMITIVE_ATTRIB_COUNT; i++)
 		{
 			vertexBuffer.putFloat(vertices[i]);
 		}
 		vertexBuffer.flip();
 		
-		VertexBuffer vbo = new VertexBuffer(s_VertexLayout, vertexBuffer, 8, VertexBufferUsage.STATIC);
+		VertexBuffer vbo = new VertexBuffer(s_VertexLayout, vertexBuffer, VERTEX_COUNT, VertexBufferUsage.STATIC);
 		
 		m_VAO.addVertexBuffer(vbo);
 		
 		int[] indices = new int[] {
-			0, 1, 1,
+			0, 1, 2,
 			2, 3, 0
 		};
 		
-		m_IBO = new IndexBuffer(indices, 24);
+		m_IBO = new IndexBuffer(indices, INDEX_COUNT);
 		
 		vbo.unbind();
 		m_IBO.unbind();
@@ -64,7 +71,6 @@ public class Renderable2D
 		
 		m_Shader = new ShaderProgram();
 		m_Shader.loadShader("res/shaders/shader.txt");
-		m_Shader.bind();
 	}
 	
 	public static VertexLayout getVertexLayout()
@@ -78,6 +84,17 @@ public class Renderable2D
 		}
 		
 		return s_VertexLayout;
+	}
+	
+	public void draw()
+	{
+		m_Shader.bind();
+		
+		m_VAO.bind();
+		m_IBO.bind();
+		
+		GL11.glDrawElements(GL11.GL_TRIANGLES, 0, null);
+		
 	}
 	
 }
