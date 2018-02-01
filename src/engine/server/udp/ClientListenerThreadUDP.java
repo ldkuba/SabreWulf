4f67ac2f43bd711c57f96909f1707c1288de2a6d
Jonas Munoz <jxm789@student.bham.ac.uk>
2018-02-01 21:39:05 +0000

    Added UDP functinality to the server

--- src/engine/server/udp/ClientListenerThreadUDP.java
+++ src/engine/server/udp/ClientListenerThreadUDP.java
@@ -1,10 +1,41 @@
 package engine.server.udp;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
 public class ClientListenerThreadUDP extends Thread{
-    public void run(){
-        while(true){
-
-        }
-
+    private static DatagramSocket UDPsocket;
+    private int port;
+    private int MAX_PACKET_SIZE;
+    private byte[] data = new byte[MAX_PACKET_SIZE];
+    
+    ClientListenerThreadUDP(int port, int packetSize) {
+    	this.port = port;
+    	MAX_PACKET_SIZE = packetSize;
     }
+    
+    public void run() {
+    	
+    	try {
+			UDPsocket = new DatagramSocket(port);
+			byte[] data = new byte[MAX_PACKET_SIZE];
+		} catch (SocketException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+    	
+    	while(true) {
+    		DatagramPacket receivePacket = new DatagramPacket(data, data.length);
+    		try {
+				UDPsocket.receive(receivePacket);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	}
+    	
+    }
+    
 }
