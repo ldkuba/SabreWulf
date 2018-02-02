e56030a12641a9140767289276613c90624a1da3
jan612 <jan612@cs.bham.ac.uk>
2018-02-02 21:51:51 +0000

    Started implementing Screen

--- src/engine/scene/Scene.java
+++ src/engine/scene/Scene.java
@@ -1,6 +1,39 @@
 package engine.scene;
 
+import java.util.ArrayList;
+
+import engine.entity.Entity;
+
 public class Scene
 {
-
+	private int m_ID;
+	private ArrayList<Entity> m_Entities;
+	
+	public Scene(int id)
+	{
+		m_ID = id;
+	}
+	
+	public void addEntity(Entity e)
+	{
+		if(!m_Entities.contains(e))
+		{
+			m_Entities.add(e);
+		}
+	}
+	
+	public void removeEntity(Entity e)
+	{
+		m_Entities.remove(e);
+	}
+	
+	public void update()
+	{
+		
+	}
+	
+	public void render()
+	{
+		
+	}
 }
