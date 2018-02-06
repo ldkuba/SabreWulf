91a6414f773e78d50131fa19437d2faf7c44f3be
bxw645 <bxw645@student.bham.ac.uk>
2018-02-06 13:46:02 +0000

    Added imports and empty methods for state classes

--- src/game/MenuState.java
+++ src/game/MenuState.java
@@ -1,5 +1,47 @@
 package game;
 
-public class MenuState {
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.opengl.GL11;
+
+import engine.state.AbstractState;
+
+public class MenuState extends AbstractState
+{
+
+	@Override
+	public void keyAction(int key, int action) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseAction(int button, int action) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deactivate() {
+		// TODO Auto-generated method stub
+		
+	}
 	//TODO: Fill in state methods to make them functional
 }
