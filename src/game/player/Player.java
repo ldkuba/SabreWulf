39078d78a80e313ec3cde90c27b36767349d17a8
bxw645 <bxw645@student.bham.ac.uk>
2018-02-08 17:05:01 +0000

    Updated Player Class

--- src/game/player/Player.java
+++ src/game/player/Player.java
@@ -1,18 +1,21 @@
 package game.player;
 
 import engine.entity.Entity;
+import engine.entity.component.*;
 
 //Will contain an Entity object with components and holds data of it's own profile (health, status, position etc)
 public class Player {
 	
 	private int id;
 	private String name;
+	private int[] position;
 	private int health;
 	private int energy;
 	String status;
 	private boolean isLocal;
 	
 	public Entity player;
+	public TransformComponent transform;
 	
 	public Player(int id, String name, boolean isLocal){
 		this.id = id;
@@ -20,21 +23,23 @@ public class Player {
 		this.isLocal = isLocal;
 		
 		player = new Entity(0, "Player " + name);
+		transform = new TransformComponent();
+		player.addComponent(transform);
 	}
 
-	public void getPosition() {
-		
+	public int[] getPosition() {
+		return position;
 	}
 
-	public void getHealth() {
-		
+	public int getHealth() {
+		return health;
 	}
 
-	public void getEnergy() {
-		
+	public int getEnergy() {
+		return energy;
 	}
 
-	public void getStatus() {
-		
+	public String getStatus() {
+		return status;
 	}
 }
