ce24b7a922ec3b24733311b63d19af344aa1c1f3
bhm699 <bhm699@cs.bham.ac.uk>
2018-02-11 17:55:06 +0000

    map stuff

--- src/game/map/Map.java
+++ src/game/map/Map.java
@@ -3,8 +3,9 @@ package game.map;
 import java.util.ArrayList;
 
 import engine.entity.Entity;
+import engine.entity.component.SpriteComponent;
 import engine.graphics.camera.Camera;
-import engine.maths.Vec3;
+import engine.maths.*;
 import engine.scene.Scene;
 
 public class Map {
@@ -13,84 +14,103 @@ public class Map {
 	Camera camera;
 	Entity shownEntity;
 	ArrayList<Entity> hiddenEntities;
-	Vec3 currentPos;
-	int i = 0; 
+	Vec2 currentPos;
+	int bkgrdPointer = 0;
 	private final int MAP_SIZE = 10;
 	Scene scene;
-	
+
 	public Map(Scene scene) {
 		this.scene = scene;
+		camera = scene.getCamera();
 		entities = new ArrayList<Entity>();
 		hiddenEntities = new ArrayList<Entity>();
 		background = new Entity[MAP_SIZE * MAP_SIZE]; // map is 10 x 10
-		//starting position of camera 
-		currentPos = new Vec3(getCameraPosX(), getCameraPosY(), getCameraPosZ());
+		// starting position of camera
+		currentPos = new Vec2(camera.getViewport().getX(), camera.getViewport().getY());
 	}
-	
+
 	public void init() {
-		// initialise entities ???
-		
-		//set hidden and current shown entity
+		// initialise entities
+		Vec4 colour1 =  new Vec4(2.0f, 3.0f, 1.0f, 0.0f);
+		Vec4 colour2 =  new Vec4(1.0f, 2.0f, 3.0f, 0.0f);
+		SpriteComponent comp1 = new SpriteComponent(colour1, 1.0f, 1.0f);
+		SpriteComponent comp2 = new SpriteComponent(colour2, 1.0f, 1.0f);
+	    for (int i = 0; i < background.length; i++){
+			Entity newEntity = new Entity(i, "");
+			if (i % 2 == 0){
+				newEntity.addComponent(comp1);
+			} else {
+				newEntity.addComponent(comp2);
+			}
+			entities.add(newEntity);
+			
+	    }
+		// set hidden and current shown entity
 		for (Entity entity : entities) {
 			hiddenEntities.add(entity);
 		}
-		shownEntity = background[i];
+		bkgrdPointer = (int) (currentPos.getX() + ((float) MAP_SIZE * currentPos.getY()));
+		shownEntity = background[bkgrdPointer];
 	}
-	
+
 	public Entity getShownEntity() {
-		//return the current visible map background
+		// return the current visible map background
 		return shownEntity;
 	}
 	
-	public void render(){
-		//get new cameraPos
-		Vec3 cameraPos = new Vec3(getCameraPosX(), getCameraPosY(), getCameraPosZ());
+	public ArrayList<Entity> getMapEntities(){
+		return entities;
+	}
+	
+	public void render() {
+		setCameraInBounds();
+		// get new cameraPos
+		Vec2 cameraPos = new Vec2(camera.getViewport().getX(), camera.getViewport().getY());
 		float diffx = cameraPos.getX() - currentPos.getX();
 		float diffy = cameraPos.getY() - currentPos.getY();
-		float diffz = cameraPos.getZ() - currentPos.getZ();
-		/* if camera has moved left the x difference will be positive
-		*                moved up the y difference will be positive
-		*/
+		/*
+		 * if camera has moved left the x difference will be positive, moved up
+		 * the y difference will be positive
+		 */
 		if (diffx > 0) {
 			moveLeft(diffx);
 		} else if (diffx < 0) {
 			moveRight(diffx);
-		} 
+		}
 		if (diffy > 0) {
 			moveUp(diffy);
 		} else if (diffy < 0) {
 			moveDown(diffy);
-		}		
+		}
 	}
 
-	private void moveLeft(float diffx){
+	private void moveLeft(float diffx) {
 		int steps = (int) diffx;
-		i = i - steps;
+		bkgrdPointer = bkgrdPointer - steps;
 		setBackground();
 	}
-	
-	private void moveRight(float diffx){
+
+	private void moveRight(float diffx) {
 		int steps = (int) diffx;
-		i = i + steps;
+		bkgrdPointer = bkgrdPointer + steps;
 		setBackground();
 	}
-	
-	private void moveUp(float diffy){
+
+	private void moveUp(float diffy) {
 		int steps = (int) diffy;
-		i = i + -(steps * MAP_SIZE);
+		bkgrdPointer = bkgrdPointer + -(steps * MAP_SIZE);
 		setBackground();
 	}
-	
-	private void moveDown(float diffy){
+
+	private void moveDown(float diffy) {
 		int steps = (int) diffy;
-		i = i + (steps * MAP_SIZE);
+		bkgrdPointer = bkgrdPointer + (steps * MAP_SIZE);
 		setBackground();
 	}
-	
-	private void setBackground(){
+
+	private void setBackground() {
 		Entity temp;
-		int i = (int) (currentPos.getX() + ((float) MAP_SIZE * currentPos.getY()));
-		temp = background[i];
+		temp = background[bkgrdPointer];
 		if (entities.contains(temp)) {
 			if (temp != shownEntity) {
 				hiddenEntities.remove(temp);
@@ -101,11 +121,11 @@ public class Map {
 			}
 		}
 	}
-		
+
 	public void setCameraInBounds() {
 		// if camera goes out of map bounds bring it back in.
 		boolean changePos = false;
-		Vec3 position = new Vec3(getCameraPosX(), getCameraPosY(), getCameraPosZ());
+		Vec3 position = new Vec3(camera.getPosition().getX(), camera.getPosition().getY(), camera.getPosition().getZ());
 		if (position.getX() > (float) MAP_SIZE / 2) {
 			position.setX((float) MAP_SIZE / 2);
 			changePos = true;
@@ -128,19 +148,7 @@ public class Map {
 			changePos = true;
 		}
 		if (changePos) {
-			camera.setPosition(position);
+			camera.move(position);
 		}
-	}
-	
-	private float getCameraPosX() {
-		return camera.getPosition().getX();
-	}
-
-	private float getCameraPosY() {
-		return camera.getPosition().getY();
-	}
-
-	private float getCameraPosZ() {
-		return camera.getPosition().getZ();
 	}
 }
