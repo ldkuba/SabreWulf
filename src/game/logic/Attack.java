f37eba592dedfbbe890ef8688352e0b5d5c3da7c
Jonas Munoz <jxm789@student.bham.ac.uk>
2018-02-16 14:26:28 +0000

    Attack Logic finished

--- src/game/logic/Attack.java
+++ src/game/logic/Attack.java
@@ -7,7 +7,7 @@ import game.player.Player;
 public class Attack extends AbstractAction{
 	
 	private Vec2 coordPlayer;
-	private Vec2 coorEnemy;
+	private Vec2 coordEnemy;
 	
 	private Vec2 noMovement = new Vec2(0,0);
 	
@@ -25,15 +25,22 @@ public class Attack extends AbstractAction{
 		} else {
 			return moveTo();
 		}
-		
 	}
 
 	public boolean inRange(Vec2 playerCoord, Vec2 enemyCoord) {
 		
-		/*
-		 * Do some calculations
-		 */
-		if(calculateDistance(playerCoord,enemyCoord) <= myPlayer.getCharClass().getAttackRange()) {
+		float rangeX = enemyCoord.getX() - playerCoord.getX();
+		float rangeY = enemyCoord.getY() - playerCoord.getY();
+		
+		//Always positive.
+		if (rangeX < 0) {
+			rangeX = rangeX * -1;
+		}
+		if(rangeY < 0) {
+			rangeY = rangeY * -1;
+		}
+		
+		if( rangeX <= myPlayer.getCharClass().getAttackRange() || rangeY <= myPlayer.getCharClass().getAttackRange()) {
 			return true;
 		} else {
 			return false;
@@ -41,16 +48,17 @@ public class Attack extends AbstractAction{
 		
 	}
 	
-	private float calculateDistance(Vec2 playerCoord, Vec2 enemyCoord) {
-		return 0.0f;
-	}
-	
-	private void doDamage() {
-		
+	public int doDamage() {
+		int damage = myPlayer.getCharClass().getAttackDmg();	//Base damage.
+		return damage;
 	}
 	
 	private Vec2 moveTo() {
+		float toX = coordPlayer.getX() - coordEnemy.getX();
+		float toY = coordPlayer.getY() - coordEnemy.getY();
+		Vec2 toPos = new Vec2(toX,toY);
 		
+		return toPos;
 	}
 	
 }
