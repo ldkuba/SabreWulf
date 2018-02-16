4fa58b19fc5cec16fd88078d24425d7ef822fcad
bhm699 <bhm699@cs.bham.ac.uk>
2018-02-16 16:04:50 +0000

    added beep into client message listener

--- src/game/client/ClientMessageListener.java
+++ src/game/client/ClientMessageListener.java
@@ -1,8 +1,12 @@
 package game.client;
 
+import org.lwjgl.openal.AL11;
+
 import engine.common_net.AbstractMessage;
 import engine.common_net.MessageListener;
 import engine.server.core.Player;
+import engine.sound.Sound;
+import engine.sound.SoundManager;
 import game.Main;
 import game.method.SetCurrentGameState;
 import game.networking.LobbyUpdateMessage;
@@ -12,19 +16,24 @@ import game.networking.LobbyConnectionResponse;
 public class ClientMessageListener implements MessageListener
 {
 	private Client client;
+	private SoundManager soundManager;
 
 	public ClientMessageListener(Client client)
 	{
 		this.client = client;
+		soundManager = new SoundManager();
 	}
 
 	@Override
 	public void receiveMessage(AbstractMessage msg, Player source)
 	{
+		String soundName = "message_beep";
 		if(msg instanceof PeerList)
 		{
+			invokeSound(soundName);
 			PeerList plm = (PeerList) msg;
 			System.out.println(plm.getNoPlayers());
+			soundManager.pauseSoundSource(soundName);
 		}
 		else if(msg instanceof LobbyConnectionResponse){
 			LobbyConnectionResponse lobbyConn = (LobbyConnectionResponse) msg;
@@ -42,5 +51,11 @@ public class ClientMessageListener implements MessageListener
 		    // Here's message containing all the players and their state
         }
 		else System.out.println(msg.getClass());
+	}
+	
+	private void invokeSound(String soundName){
+		this.soundManager.init();
+		this.soundManager.setAttenuationModel(AL11.AL_EXPONENT_DISTANCE);
+		Sound.setupSounds(soundManager, "res/sounds/beep.ogg", soundName);
 	}
 }
