5ff681f10effd6d7c4a438e67019da1c735be627
shw684 <shw684@cs.bham.ac.uk>
2018-02-18 15:19:06 +0000

    initial commit of branch ui

--- src/engine/font/Font.java
+++ src/engine/font/Font.java
@@ -7,72 +7,66 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 import org.lwjgl.BufferUtils;
-import org.lwjgl.stb.STBImageWrite;
 import org.lwjgl.stb.STBTTAlignedQuad;
-import org.lwjgl.stb.STBTTPackContext;
-import org.lwjgl.stb.STBTTPackedchar;
 import org.lwjgl.stb.STBTruetype;
 import org.lwjgl.system.MemoryStack;
 
 import engine.graphics.texture.Texture;
 import engine.maths.Vec2;
 
-public class Font
-{
+public class Font {
 	private Texture m_Texture;
-	private STBTTPackedchar.Buffer m_CharData;
 	private String m_Name;
 
-	public Font(String name)
-	{
+	public Font(String name) {
 		m_Name = name;
 
 		try (MemoryStack stack = MemoryStack.stackPush()) {
 			Path p = FileSystems.getDefault().getPath("res/fonts", name);
 			byte[] fontBytes = new byte[0];
-			try
-			{
+			try {
 				fontBytes = Files.readAllBytes(p);
-			}catch (IOException e)
-			{
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
-	
+
 			ByteBuffer fontBuffer = BufferUtils.createByteBuffer(fontBytes.length);
 			fontBuffer.put(fontBytes, 0, fontBytes.length);
-	
+
 			ByteBuffer bitmap = BufferUtils.createByteBuffer(1024 * 1024);
-//	
-//			m_CharData = STBTTPackedchar.malloc(3 * 128);
-//	
-//			STBTTPackContext pc = STBTTPackContext.malloc();
-//			STBTruetype.stbtt_PackBegin(pc, bitmap, 1024, 1024, 0, 1);
-//	
-//			m_CharData.position(0 * 128 + 32);
-//			STBTruetype.stbtt_PackSetOversampling(pc, 1, 1);
-//			STBTruetype.stbtt_PackFontRange(pc, fontBuffer, 0, 24.0f, 32, m_CharData);
-//	
-//			m_CharData.position(1 * 128 + 32);
-//			STBTruetype.stbtt_PackSetOversampling(pc, 2, 2);
-//			STBTruetype.stbtt_PackFontRange(pc, fontBuffer, 0, 24.0f, 32, m_CharData);
-//	
-//			m_CharData.position(2 * 128 + 32);
-//			STBTruetype.stbtt_PackSetOversampling(pc, 3, 1);
-//			STBTruetype.stbtt_PackFontRange(pc, fontBuffer, 0, 24.0f, 32, m_CharData);
-//	
-//			STBTruetype.stbtt_PackEnd(pc);
-//			pc.free();
-//	
-//			CharSequence cs = "res/fontmap.png";
-//	
-//			STBImageWrite.stbi_write_png(cs, 512, 512, 1, bitmap, 4);
-//	
-//			m_Texture = new Texture(bitmap, 512, 512, name);
+			//
+			// m_CharData = STBTTPackedchar.malloc(3 * 128);
+			//
+			// STBTTPackContext pc = STBTTPackContext.malloc();
+			// STBTruetype.stbtt_PackBegin(pc, bitmap, 1024, 1024, 0, 1);
+			//
+			// m_CharData.position(0 * 128 + 32);
+			// STBTruetype.stbtt_PackSetOversampling(pc, 1, 1);
+			// STBTruetype.stbtt_PackFontRange(pc, fontBuffer, 0, 24.0f, 32,
+			// m_CharData);
+			//
+			// m_CharData.position(1 * 128 + 32);
+			// STBTruetype.stbtt_PackSetOversampling(pc, 2, 2);
+			// STBTruetype.stbtt_PackFontRange(pc, fontBuffer, 0, 24.0f, 32,
+			// m_CharData);
+			//
+			// m_CharData.position(2 * 128 + 32);
+			// STBTruetype.stbtt_PackSetOversampling(pc, 3, 1);
+			// STBTruetype.stbtt_PackFontRange(pc, fontBuffer, 0, 24.0f, 32,
+			// m_CharData);
+			//
+			// STBTruetype.stbtt_PackEnd(pc);
+			// pc.free();
+			//
+			// CharSequence cs = "res/fontmap.png";
+			//
+			// STBImageWrite.stbi_write_png(cs, 512, 512, 1, bitmap, 4);
+			//
+			// m_Texture = new Texture(bitmap, 512, 512, name);
 		}
 	}
 
-	public Vec2[] getUVs(char c)
-	{
+	public Vec2[] getUVs(char c) {
 		Vec2[] resultUV = new Vec2[4];
 
 		STBTTAlignedQuad quad = STBTTAlignedQuad.malloc();
@@ -95,18 +89,15 @@ public class Font
 		return resultUV;
 	}
 
-	public Texture getTexture()
-	{
+	public Texture getTexture() {
 		return m_Texture;
 	}
 
-	public String getName()
-	{
+	public String getName() {
 		return m_Name;
 	}
 
-	public void destroy()
-	{
+	public void destroy() {
 		m_CharData.free();
 	}
 }
