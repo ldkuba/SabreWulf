af1da0fd7e7a9c692c7318df8c77a96916a2e6bb
shw684 <shw684@cs.bham.ac.uk>
2018-02-18 14:42:18 +0000

    to be done

---
+++ src/game/logic/Damage.java
@@ -0,0 +1,12 @@
+package game.logic;
+
+import engine.net.server.core.Player;
+
+public class Damage {
+
+	public int lostVitality(Player opponent, Player myPlayer) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
--- src/game/logic/GameLogic.java
+++ src/game/logic/GameLogic.java
@@ -1,31 +1,30 @@
 package game.logic;
 
 import engine.maths.Vec2;
-import game.logic.Attack;
-import game.logic.Movement;
-import game.player.Player;
+import engine.net.server.core.Player;
 
 public class GameLogic {
-	
+
 	private Attack attackAction = new Attack();
+	private Damage recvDamage = new Damage();
 	private Movement movement = new Movement();
-	
+
 	public static Player myPlayer;
 
 	public GameLogic() {
-		
+
 	}
-	
+
 	public int doAttack(Vec2 playCoord, Vec2 enemyCoord) {
 		return attackAction.attack(playCoord, enemyCoord, myPlayer.getCharClass().getAttackRange());
 	}
-	
+
 	public float doMovement() {
 		return movement.move();
 	}
-	
+
 	public int receiveDamage(Player opponent, Player myPlayer) {
 		return recvDamage.lostVitality(opponent, myPlayer);
 	}
-	
+
 }
