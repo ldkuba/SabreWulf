f73ba68cacd1f3c395df309ee2595efe228b5fa3
bhm699 <bhm699@cs.bham.ac.uk>
2018-02-18 14:34:03 +0000

    removed some hardcoding/for some reason it reverted back to the old map class

--- src/game/common/map/Map.java
+++ src/game/common/map/Map.java
@@ -17,6 +17,7 @@ public class Map {
 	private final float TILE_WIDTH = 1.8f;
 	private final float TILE_HEIGHT = 1.0f;
 	private final int MAP_SIZE = 10;
+	private final int ARRAY_SIZE = MAP_SIZE * MAP_SIZE * 4;
 	private Scene scene;
 	private float vpLength;
 
@@ -26,25 +27,17 @@ public class Map {
 		visible = new ArrayList<Entity>();
 		background = new Entity[MAP_SIZE * MAP_SIZE * 4];
 		vpLength = Application.s_Viewport.getLength();
-		init();
 	}
 
 	public void init() {
+		System.out.println("here");
 		// initialise entities - currently just test values
 		Vec4 grey = new Vec4(0.4f, 0.4f, 0.4f, 1.0f);
 		Vec4 pink = new Vec4(0.9f, 0.3f, 0.5f, 1.0f);
 		Vec4 brown = new Vec4(0.4f, 0.1f, 0.0f, 1.0f);
 		Vec4 blue = new Vec4(0.3f, 0.4f, 0.7f, 1.0f);
-		Vec4 x = new Vec4(0.4f, 0.1f, 0.8f, 1.0f);
-		
-		Entity origin = new Entity(0, "");
-		SpriteComponent comp = new SpriteComponent(x, TILE_WIDTH, TILE_HEIGHT);
-		origin.addComponent(comp);
-		origin.addComponent(new TransformComponent());
-		origin.getTransform().setPosition(new Vec3(0, 0, 0.0f));
-		background[0] = origin;
 		// top right
-		for (int i = 1; i < background.length / 4; i++) {
+		for (int i = 0; i < background.length / 4; i++) {
 			Entity newEntity = new Entity(i, "");
 			SpriteComponent comp1 = new SpriteComponent(brown, TILE_WIDTH, TILE_HEIGHT);
 			newEntity.addComponent(comp1);
@@ -53,8 +46,8 @@ public class Map {
 			background[i] = newEntity;
 		}
 		// bottom left
-		for (int i = 100; i < background.length / 2; i++) {
-			SpriteComponent comp1 = null;
+		for (int i = (ARRAY_SIZE/4); i < background.length / 2; i++) {
+			SpriteComponent comp1 = new SpriteComponent(grey, TILE_WIDTH, TILE_HEIGHT);;
 			Entity newEntity = new Entity(i, "");
 			comp1 = new SpriteComponent(pink, TILE_WIDTH, TILE_HEIGHT);
 			newEntity.addComponent(comp1);
@@ -63,8 +56,8 @@ public class Map {
 			background[i] = newEntity;
 		}
 		// bottom right
-		for (int i = 200; i < (background.length*3/4); i++) {
-			SpriteComponent comp1 = null;
+		for (int i = (ARRAY_SIZE/2); i < (background.length*3/4); i++) {
+			SpriteComponent comp1 = new SpriteComponent(pink, TILE_WIDTH, TILE_HEIGHT);;
 			Entity newEntity = new Entity(i, "");
 			comp1 = new SpriteComponent(blue, TILE_WIDTH, TILE_HEIGHT);
 			newEntity.addComponent(comp1);
@@ -73,8 +66,8 @@ public class Map {
 			background[i] = newEntity;
 		}
 		// top left
-		for (int i = 300; i < background.length; i++) {
-			SpriteComponent comp1 = null;
+		for (int i = (ARRAY_SIZE*3/4); i < background.length; i++) {
+			SpriteComponent comp1 = new SpriteComponent(blue, TILE_WIDTH, TILE_HEIGHT);;
 			Entity newEntity = new Entity(i, "");
 			comp1 = new SpriteComponent(grey, TILE_WIDTH, TILE_HEIGHT);
 			newEntity.addComponent(comp1);
@@ -82,7 +75,6 @@ public class Map {
 			newEntity.getTransform().setPosition(new Vec3(0 - TILE_WIDTH * (i % MAP_SIZE), 0 + TILE_HEIGHT * ((i - 300) / MAP_SIZE), 0.0f));
 			background[i] = newEntity;
 		}
-		
 		for (int i = 0; i < background.length; i++) {
 			scene.addEntity(background[i]);
 			visible.add(background[i]);
@@ -97,24 +89,24 @@ public class Map {
 		float rightX = x + vpLength;
 		float upperY = y + vpLength;
 		float lowerY = y - vpLength;
-		System.out.println("view port");
+		/*System.out.println("view port");
 		System.out.println(leftX);
 		System.out.println(rightX);
 		System.out.println(upperY);
-		System.out.println(lowerY);
-		for (int i = 0; i < 1; i++) {
+		System.out.println(lowerY);*/
+		for (int i = 0; i < background.length; i++) {
 			Entity temp = background[i];
 			Vec3 pos = temp.getTransform().getPosition();
-			// assuming that pos gives the bottom left
-			float minX = pos.getX(); //entity pos
-			float minY = pos.getY(); //entity pos
+			// pos gives the bottom left of entity
+			float minX = pos.getX(); 
+			float minY = pos.getY(); 
 			float maxX = minX + temp.getSprite().getWidth();
 			float maxY = minY + temp.getSprite().getHeight();
-			System.err.println("hi");
+         /*	System.err.println("hi");
 			System.out.println(minX);
 			System.out.println(minY);
 			System.out.println(maxX);
-			System.out.println(maxY);
+			System.out.println(maxY);*/
 			//these need to change
 			if ((minX <= rightX && minX >= leftX) || (maxX <= rightX && maxX >= leftX)) {
 				if (!visible.contains(temp)) {
