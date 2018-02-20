46174c2f99a5484404de134d3ebc8ce909a60fd0
bxw645 <bxw645@student.bham.ac.uk>
2018-02-20 15:25:19 +0000

    cleaned up edge generation method

--- src/engine/AI/Navmesh.java
+++ src/engine/AI/Navmesh.java
@@ -19,29 +19,34 @@ public class Navmesh {
 		Edge edge;
 		
 		for(int i = 0; i < triangles.size(); i++){
+			
 			A = triangles.get(i);
+			
 			for(int j = 0; j < triangles.size(); j++){
 				
 				B = triangles.get(j);
 				
-				boolean xx = A.getX() == B.getX();
-				boolean xy = A.getX() == B.getY();
-				boolean xz = A.getX() == B.getZ();
-				boolean yx = A.getY() == B.getX();
-				boolean yy = A.getY() == B.getY();
-				boolean yz = A.getY() == B.getZ();
-				boolean zx = A.getZ() == B.getX();
-				boolean zy = A.getZ() == B.getY();
-				boolean zz = A.getZ() == B.getZ();
+				boolean[] boollist = new boolean[9]; 
 				
-				if((xx && xy)||(xx && xz)||(xx && yx)||(xx && yy)||(xx && yz)||(xx && zx)||(xx && zy)||(xx && zz)||
-				   (xy && xz)||(xy && yx)||(xy && yy)||(xy && yz)||(xy && zx)||(xy && zy)||(xy && zz)||
-				   (xz && yx)||(xz && yy)||(xz && yz)||(xz && zx)||(xz && zy)||(xz && zz)||
-			       (yx && yy)||(yx && yz)||(yx && zx)||(yx && zy)||(yx && zz)||
-				   (yy && yz)||(yy && zx)||(yy && zy)||(yy && zz)||
-				   (yz && zx)||(yz && zy)||(yz && zz)||
-				   (zx && zy)||(zx && zz)){
-					
+				boollist[0] = A.getX() == B.getX();
+				boollist[1] = A.getX() == B.getY();
+				boollist[2] = A.getX() == B.getZ();
+				boollist[3] = A.getY() == B.getX();
+				boollist[4] = A.getY() == B.getY();
+				boollist[5] = A.getY() == B.getZ();
+				boollist[6] = A.getZ() == B.getX();
+				boollist[7] = A.getZ() == B.getY();
+				boollist[8] = A.getZ() == B.getZ();
+				
+				int count = 0;
+				
+				for(int k = 0; k < boollist.length; k++){
+					if(boollist[k]){
+						count++;
+					}
+				}
+				
+				if(count >= 2){
 					edge = new Edge(A.getMidpoint(), B.getMidpoint());
 					edges.add(edge);
 				}
