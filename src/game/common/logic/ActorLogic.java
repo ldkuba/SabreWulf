886eb28c04c71ef13fb8afd30d511a479704b825
Jonas Munoz <jxm789@student.bham.ac.uk>
2018-03-01 21:45:31 +0000

    Minor thing with logic

--- src/game/common/logic/ActorLogic.java
+++ src/game/common/logic/ActorLogic.java
@@ -3,6 +3,7 @@ package game.common.logic;
 import engine.maths.Vec2;
 import engine.net.server.core.NetPlayer;
 import engine.application.Application;
+import engine.entity.Entity;
 import game.common.logic.actions.Movement;
 import game.common.logic.actions.Respawn;
 import game.common.actors.Player;
@@ -12,15 +13,19 @@ public class ActorLogic {
 	private Movement movement = new Movement();
 	private Respawn respawn = new Respawn();
 	
-	protected static NetPlayer myActor;
+	protected static Player myActor;
+	protected static NetPlayer myNetActor;
+	
+	protected static Entity myEntity;
 
-	public ActorLogic(NetPlayer myPlayer) {
+	public ActorLogic(Player myPlayer) {
 		ActorLogic.myActor = myPlayer;
+		myEntity = myActor.getEntity();
 	}
 	
 	public boolean attack(Vec2 playCoord, Vec2 enemyCoord, Player enemy) {
 		if(inRange(playCoord, enemyCoord, myActor.getAttackRange())) {
-			float dmg = myActor.getDamage();
+			float dmg = myEntity.getComponent(NetDataComponent);
 			enemy.getLogic().receiveDamage(dmg);	//Replaced by sending it to the server.
 			//Display damage.
 			return true;
