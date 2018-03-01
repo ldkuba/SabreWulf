603669d8eb04e055e377b9bb924039652c522441
shw684 <shw684@cs.bham.ac.uk>
2018-03-01 12:14:31 +0000

    minor changes to respawn

--- src/game/common/logic/actions/Respawn.java
+++ src/game/common/logic/actions/Respawn.java
@@ -1,26 +1,31 @@
 package game.common.logic.actions;
 
 import engine.maths.Vec2;
+import game.common.actors.Player;
 
 public class Respawn {
 
 	private Player myPlayer;
-	Vec2 spawnCoord = new Vec2(0, 0); // zero to be replaced by coords of spawn
+	Vec2 spawnCoord = new Vec2(0, 0); // zero to be replaced by coords of
+										// spawn
 										// room
 
 	public Respawn() {
 
 	}
 
-	public boolean zeroVitality() {
-		if (myPlayer.getRole.getHealth <= 0) {
+	public boolean zeroHealth() {
+		if (myPlayer.getHealth() <= 0) {
 			return true;
 		}
+		return false;
 	}
 
-	public Vec2 respawn() {
-		if (zeroVitality() == true) {
-			myPlayer.setCoord(spawnCoord);
+	public void respawn() {
+		if (zeroHealth() == true) {
+			// do a cooldown timer
+			myPlayer.setPosition(spawnCoord);
 		}
+		// some way to set the player health to max
 	}
 }
